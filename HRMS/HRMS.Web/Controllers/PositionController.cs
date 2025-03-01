b71346738a316c292a3dcbb55ebf54e11ae03490
MG <akog02@gmail.com>
2025-03-01 21:21:19 +0700

    add Delete,Update function and use datatable.net in hrms project.

--- HRMS/HRMS.Web/Controllers/PositionController.cs
+++ HRMS/HRMS.Web/Controllers/PositionController.cs
@@ -46,11 +46,62 @@ namespace HRMS.Web.Controllers {
 
         public IActionResult List() {
             IList<PositionViewModel> positionViews = _db.Positions.Where(w => w.IsActive).Select(s => new PositionViewModel {
+                Id = s.Id,
                 Code = s.Code,
                 Description = s.Description,
                 Level = s.Level.Value
             }).ToList();
             return View(positionViews);
+        }
+        //postion/delete?Id=1
+        public IActionResult Delete(string Id) {
+            try {
+                PositionEntity positionEntity = _db.Positions.Where(w => w.Id == Id).FirstOrDefault();
+                if (positionEntity is not null) {
+                    positionEntity.IsActive = false;
+                    _db.SaveChanges();
+                    TempData["Msg"] = "Data has been deleted successfully";
+                    TempData["IsErrorOccur"] = false;
+                }
+            }
+            catch (Exception e) {
+                TempData["Msg"] = "Oh,Sorry error was occured when recrod is deleted time.";
+                TempData["IsErrorOccur"] = true;
+            }
+            return RedirectToAction("List");
+        }
+
+        public IActionResult Edit(string Id) {
+            PositionViewModel positionView = _db.Positions.Where(w => w.IsActive && w.Id == Id).Select(s => new PositionViewModel {
+                Id = s.Id,
+                Code = s.Code,
+                Description = s.Description,
+                Level = s.Level.Value
+            }).FirstOrDefault();
+            return View(positionView);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(PositionViewModel positionVM) {
+            try {
+                //DTO >> Data Transfer Object in hre ( from View Models to Data Model)
+                PositionEntity existingPositionEntity = _db.Positions.Where(w => w.IsActive && w.Id == positionVM.Id).FirstOrDefault();
+                if (existingPositionEntity is not null) {
+                    existingPositionEntity.Description = positionVM.Description;
+                    existingPositionEntity.Level = positionVM.Level;
+                    existingPositionEntity.UpdatedAt = DateTime.Now;
+                    existingPositionEntity.UpdatedBy = "system";
+                    existingPositionEntity.Ip = await NetworkHelper.GetIpAddressAsnyc();
+                }
+                _db.Positions.Update(existingPositionEntity);//updating the recrod to the context
+                _db.SaveChanges();//saving the data to the database in here .
+                TempData["Msg"] = "Data has been updated successfully";
+                TempData["IsErrorOccur"] = false;
+            }
+            catch (Exception e) {
+                TempData["Msg"] = "Oh,Sorry error was occured when recrod is updated time.";
+                TempData["IsErrorOccur"] = true;
+            }
+            return RedirectToAction("List");
         }
     }
 }
--- HRMS/HRMS.Web/Models/ViewModels/PositionViewModel.cs
+++ HRMS/HRMS.Web/Models/ViewModels/PositionViewModel.cs
@@ -1,5 +1,6 @@
 ﻿namespace HRMS.Web.Models.ViewModels {
     public class PositionViewModel {
+        public string Id { get; set; }//for delete/update  process 
         public string Code { get; set; }
         public string Description { get; set; }
         public int Level { get; set; }
